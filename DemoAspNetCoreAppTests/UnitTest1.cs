d2ffe5b8be8849581332e667f97ab2d6bbde279f
Kaleb Pederson <kaleb.pederson@gmail.com>
2019-08-30 23:11:14 -0600

    Add unit test project.

---
+++ DemoAspNetCoreAppTests/UnitTest1.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+
+namespace DemoAspNetCoreAppTests
+{
+  public class UnitTest1
+  {
+    [Fact]
+    public void First_test()
+    {
+      Assert.Equal(1, 1);
+    }
+    
+    [Fact]
+    public void Second_test()
+    {
+      Assert.Equal(1, 1);
+    }
+  }
+}
